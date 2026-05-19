848d5e31c0063491a128d9a1f1be8555f8178cc3
danexdigital <danexdigitalservices@gmail.com>
2026-05-19 14:20:57 +0000

    good

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
       <Header />
-      <main className="pt-24">
+      <main className="pt-20 md:pt-22">
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/services" element={<Services />} />
